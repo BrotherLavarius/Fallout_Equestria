76201de93fb2554284b3d4d715e127669200f738
Hoijima <hoijima@sky-sanctuary.ru>
2016-07-23 20:29:35 +0300

    started saving metadata of it's facing

--- java/com/redsparkle/foe/blocks/SparkleColaMachineBlock.java
+++ java/com/redsparkle/foe/blocks/SparkleColaMachineBlock.java
@@ -1,7 +1,6 @@
 package com.redsparkle.foe.blocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
@@ -9,22 +8,21 @@ import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.InventoryBasic;
-import net.minecraft.inventory.InventoryLargeChest;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.obj.OBJModel;
 import net.minecraftforge.common.property.ExtendedBlockState;
 import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.common.property.IUnlistedProperty;
 import net.minecraftforge.common.property.Properties;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static com.redsparkle.foe.main.MODID;
 
@@ -36,6 +34,7 @@ public class SparkleColaMachineBlock extends Block {
     public static final SparkleColaMachineBlock instance = new SparkleColaMachineBlock();
     public static final String name = "SparkleColaMachine";
     private int counter = 1;
+    private ExtendedBlockState state = new ExtendedBlockState(this, new IProperty[]{FACING}, new IUnlistedProperty[]{OBJModel.OBJProperty.INSTANCE});
 
     private SparkleColaMachineBlock()
     {
@@ -47,6 +46,27 @@ public class SparkleColaMachineBlock extends Block {
         setRegistryName(new ResourceLocation(MODID, name));
 
     }
+
+    public static EnumFacing getFacingFromEntity(World worldIn, BlockPos clickedBlock, EntityLivingBase entityIn)
+    {
+        if (MathHelper.abs((float)entityIn.posX - (float)clickedBlock.getX()) < 2.0F && MathHelper.abs((float)entityIn.posZ - (float)clickedBlock.getZ()) < 2.0F)
+        {
+            double d0 = entityIn.posY + (double)entityIn.getEyeHeight();
+
+            if (d0 - (double)clickedBlock.getY() > 2.0D)
+            {
+                return EnumFacing.UP;
+            }
+
+            if ((double)clickedBlock.getY() - d0 > 0.0D)
+            {
+                return EnumFacing.DOWN;
+            }
+        }
+
+        return entityIn.getHorizontalFacing().getOpposite();
+    }
+
     public TileEntity createNewTileEntity(World worldIn, int meta)
     {
         return new SparkleColaMachineTileEntity();
@@ -81,8 +101,39 @@ public class SparkleColaMachineBlock extends Block {
     @Override
     public BlockStateContainer createBlockState()
     {
-        return new ExtendedBlockState(this, new IProperty[0], new IUnlistedProperty[] {Properties.AnimationProperty});
+        return new ExtendedBlockState(this, new IProperty[]{FACING}, new IUnlistedProperty[] {Properties.AnimationProperty});
     }
+
+    @Override
+    public IBlockState onBlockPlaced(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        return this.getDefaultState().withProperty(FACING, getFacingFromEntity(world, pos, placer));
+    }
+
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(FACING).getIndex();
+    }
+
+    public IBlockState getStateFromMeta(int meta)
+    {
+        EnumFacing enumfacing = EnumFacing.getFront(meta);
+
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y)
+        {
+            enumfacing = EnumFacing.NORTH;
+        }
+
+        return this.getDefaultState().withProperty(FACING, enumfacing);
+    }
+
+        @SideOnly(Side.CLIENT)
+    public IBlockState getStateForEntityRender(IBlockState state)
+    {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.NORTH);
+    }
+
+
 }
 
 
