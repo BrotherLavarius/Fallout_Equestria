47a7e0dff27e4589a07d663fd67979193a4a9869
Hoijima <NENYNAX@gmail.com>
2016-08-02 21:07:02 +0300

    more radiation block

--- java/com/redsparkle/foe/block/effectDispenser/RadiationBlock.java
+++ java/com/redsparkle/foe/block/effectDispenser/RadiationBlock.java
@@ -5,23 +5,28 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ChatLine;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.EnumBlockRenderType;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.Random;
 
 import static com.redsparkle.foe.main.MODID;
+import static sun.audio.AudioPlayer.player;
 
 /**
  * Created by hoijima desu on 29.07.16 desu.
@@ -31,6 +36,11 @@ public class RadiationBlock extends Block {
     public static final String name = "RadiationBlock";
     private final boolean isOn = true;
     int collide_rad = 0;
+
+    protected static final AxisAlignedBB CACTUS_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.9375D, 0.9375D);
+    protected static final AxisAlignedBB CACTUS_COLLISION_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 1.0D, 0.9375D);
+    EntityPlayer entity = Minecraft.getMinecraft().thePlayer;
+
     int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0;
 
     boolean red = true;
@@ -67,9 +77,18 @@ public class RadiationBlock extends Block {
 
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return new AxisAlignedBB(-2.0D, -2.0D, -2.0D, 2.0D, 2.0D, 2.0D);
+        return new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+    }
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos)
+    {
+        return CACTUS_AABB;
     }
 
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos)
+    {
+        return CACTUS_COLLISION_AABB.offset(pos);
+    }
 
     @Override
     public int tickRate(World world) {
@@ -78,7 +97,7 @@ public class RadiationBlock extends Block {
 
     @Override
     public int quantityDropped(Random par1Random) {
-        return 1;
+        return 0;
     }
 
     public EnumBlockRenderType getRenderType(IBlockState state)
@@ -87,10 +106,6 @@ public class RadiationBlock extends Block {
     }
 
     @Nullable
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos)
-    {
-        return null;
-    }
 
     /**
      * Used to determine ambient occlusion and culling when rebuilding chunks for render
@@ -102,7 +117,7 @@ public class RadiationBlock extends Block {
 
     public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid)
     {
-        return true;
+        return this.isCollidable();
     }
 
     /**
@@ -139,15 +154,21 @@ public class RadiationBlock extends Block {
                 double d2 = (double) ((float) par4 + 0.5F) + (double) (par5Random.nextFloat() - 0.5F) * 1.9D * 20;
                 double d3 = 0.2199999988079071D;
                 double d4 = 0.27000001072883606D;
-                par1World.spawnParticle(EnumParticleTypes.FIREWORKS_SPARK, d0 - d4, d1 + d3, d2, 0.0D, 0.0D, 0.0D);
+                par1World.spawnParticle(EnumParticleTypes.CRIT_MAGIC, d0 - d4, d1 + d3, d2, 0.0D, 0.0D, 0.0D);
             }
 
     }
-    public void onEntityCollidedWithBlock( World world, int x, int y, int z, Entity entity ) {
-        if (entity instanceof EntityLiving) {
-            collide_rad++;
-            System.out.println("+"+collide_rad+"rads");
-            entity.attackEntityFrom(null, 1);
-        }
+
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn)
+    {
+        //collide_rad++;
+        //System.out.println("+"+collide_rad+"rads");
+
+    }
+
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
+    {
+        collide_rad++;
+        System.out.println("+"+collide_rad+"rads");
     }
 }
