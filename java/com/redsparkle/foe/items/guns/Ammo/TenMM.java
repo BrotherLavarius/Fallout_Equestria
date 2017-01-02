b658d1c7011e29d7a3bd9bf2bd7e3468f3658820
Hoijima <nenynax@gmail.com>
2017-01-02 12:02:37 +0300

    making 10mm pistol and rounds

--- java/com/redsparkle/foe/items/guns/Ammo/TenMM.java
+++ java/com/redsparkle/foe/items/guns/Ammo/TenMM.java
@@ -1,10 +1,31 @@
 package com.redsparkle.foe.items.guns.Ammo;
 
+import com.redsparkle.foe.creativeTabs.InitCreativeTabs;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 /**
  * Created by hoijima desu on 29.07.16 desu.
  */
 public class TenMM extends Item {
+    public TenMM()
+    {
+        final int NUMBER_OF_BOXES = 10;
+        this.setMaxStackSize(NUMBER_OF_BOXES);
+        this.setCreativeTab(InitCreativeTabs.Fallout_ammo);   // the item will appear on the Miscellaneous tab in creative
+    }
 
+    /**
+     * allows items to add custom lines of information to the mouseover description
+     */
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced)
+    {
+    }
 }
