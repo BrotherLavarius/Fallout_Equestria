5b57579629171db284b8e67517a995c4ff560c6e
Hoijima <nenynax@gmail.com>
2017-01-09 13:47:04 +0300

    working on guns and stuff

---
+++ java/com/redsparkle/foe/capa/rads/IRadiationCapability.java
@@ -0,0 +1,27 @@
+package com.redsparkle.foe.capa.rads;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+/**
+ * Created by hoijima on 08.09.16.
+ */
+public interface IRadiationCapability  {
+
+
+    public Integer addRadiation(Integer addRadiationLevel);
+
+    public Integer removeRadiation(Integer removeRadiationLevel);
+
+    public Integer getRadiation();
+
+    public Integer setRadiation(Integer newRadiationLevel);
+
+    public void update(EntityPlayer player, World world, TickEvent.Phase phase);
+
+    public void updateClient(EntityPlayer player);
+
+    public void timedRemoveRad(EntityPlayer player,TickEvent.Phase phase,Integer startCycle,Integer Cycles, Integer FortificationValue);
+
+}
