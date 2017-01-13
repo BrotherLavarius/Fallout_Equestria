83aeaa0339eb92e4c79cc2fd87de4dfb0c8e0896
Hoijima <nenynax@gmail.com>
2017-01-13 18:07:12 +0300

    reworking the bullets

--- java/com/redsparkle/foe/events/EventHandlerPost.java
+++ java/com/redsparkle/foe/events/EventHandlerPost.java
@@ -1,7 +1,11 @@
 package com.redsparkle.foe.events;
 
+/**
+ * Created by hoijima on 07.09.16.
+ */
 
-public class EventHandlerPost {
+public class EventHandlerPost{
+    //nothing to do here
 }
 
 
