b04667bb1eedf174f87e58224cb8ea132671abc0
Hoijima <nenynax@gmail.com>
2017-02-18 14:58:01 +0300

    :|

--- src/com/foeSite/client/FoeSiteServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.foeSite.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface FoeSiteServiceAsync {
-    void getMessage(String msg, AsyncCallback<String> async);
-}
