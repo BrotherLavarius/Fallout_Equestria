c9287ccbc8416dae183daa8a803326fd28e612e8
Hoijima <nenynax@gmail.com>
2017-03-16 21:16:00 +0300

    pipbuck SOON(TM)

--- java/com/redsparkle/foe/keys/KeyInputHandler.java
+++ java/com/redsparkle/foe/keys/KeyInputHandler.java
@@ -4,6 +4,7 @@ import com.redsparkle.foe.gui.PipBuckGui;
 import com.redsparkle.foe.main;
 import com.redsparkle.foe.network.MessageGunReload;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
@@ -15,17 +16,18 @@ public class KeyInputHandler
 {
     private static PipBuckGui pipBuckGui;
     public Integer activated = 0;
-    public Minecraft mc = Minecraft.getMinecraft();
-    public EntityPlayer player = Minecraft.getMinecraft().player;
+
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent event)
     {
+         Minecraft mc = Minecraft.getMinecraft();
+         EntityPlayerSP player = mc.player;
             if (keyHandler.reload.isPressed()){
                 main.simpleNetworkWrapper.sendToServer(new MessageGunReload());
             }
             if (keyHandler.pipbuck.isPressed()){
 
-                player.openGui(main.instance, 0, mc.world, (int) player.posX, (int) player.posY, (int) player.posZ);;
+                player.openGui(main.instance, 0, mc.world, (int) mc.player.posX, (int) mc.player.posY, (int) mc.player.posZ);;
 //TODO: Actually fix this shit, AND I MEAN FIX!
 
             }
