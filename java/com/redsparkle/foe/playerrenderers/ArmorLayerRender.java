229600e569a061738c7784c9b516bd61d56ce083
Hoijima <nenynax@gmail.com>
2017-03-26 01:42:47 +0300

    rofl messages

--- java/com/redsparkle/foe/playerrenderers/ArmorLayerRender.java
+++ java/com/redsparkle/foe/playerrenderers/ArmorLayerRender.java
@@ -56,7 +56,7 @@ public class ArmorLayerRender implements LayerRenderer<EntityLivingBase> {
 
 
 
-            GlStateManager.translate(0.115F, 0.15F, 0.15F);
+            GlStateManager.translate(0F, 0F, 0F);
             GlStateManager.rotate(netHeadYaw, 0, 1.0F, 0);
             GlStateManager.rotate(headPitch, 1.0F, 0, 0);
 
