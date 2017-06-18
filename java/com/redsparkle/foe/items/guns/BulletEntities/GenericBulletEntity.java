c17a0371e4bd35f4117ad158e82d7e4c97288bab
hoijima <nenynax@gmail.com>
2017-06-18 19:28:46 +0300

    Working on correct gun mechanics

---
+++ java/com/redsparkle/foe/items/guns/BulletEntities/GenericBulletEntity.java
@@ -0,0 +1,143 @@
+package com.redsparkle.foe.items.guns.BulletEntities;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IProjectile;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * Created by hoijima on 18.06.17.
+ */
+public abstract class GenericBulletEntity extends Entity implements IProjectile {
+    private int ticksInGround;
+    private int xTile;
+    private int yTile;
+    private int zTile;
+    private double damage;
+
+
+    public int damageln;
+
+    public GenericBulletEntity(World worldIn)
+    {
+        super(worldIn);
+        this.xTile = -1;
+        this.yTile = -1;
+        this.zTile = -1;
+        this.damage = damageln;
+        this.setSize(0.5F, 0.5F);
+    }
+    public GenericBulletEntity(World worldIn, EntityLivingBase shooter) {
+        super(worldIn);
+    }
+
+
+
+    @Override
+    protected void entityInit() {
+
+    }
+
+    /**
+     * (abstract) Protected helper method to read subclass entity data from NBT.
+     *
+     * @param compound
+     */
+    @Override
+    protected void readEntityFromNBT(NBTTagCompound compound) {
+
+    }
+
+    /**
+     * (abstract) Protected helper method to write subclass entity data to NBT.
+     *
+     * @param compound
+     */
+    @Override
+    protected void writeEntityToNBT(NBTTagCompound compound) {
+
+    }
+    /**
+     * Whether the arrow has a stream of critical hit particles flying behind it.
+     */
+    public boolean getIsCritical()
+    {
+        return false;
+    }
+    /**
+     * Similar to setArrowHeading, it's point the throwable entity to a x, y, z direction.
+     *
+     * @param x
+     * @param y
+     * @param z
+     * @param velocity
+     * @param inaccuracy
+     */
+    /**
+     * Checks if the entity is in range to render.
+     */
+    @SideOnly(Side.CLIENT)
+    public boolean isInRangeToRenderDist(double distance)
+    {
+        double d0 = this.getEntityBoundingBox().getAverageEdgeLength() * 10.0D;
+
+        if (Double.isNaN(d0))
+        {
+            d0 = 1.0D;
+        }
+
+        d0 = d0 * 64.0D * getRenderDistanceWeight();
+        return distance < d0 * d0;
+    }
+
+    public void setAim(Entity shooter, float pitch, float yaw, float p_184547_4_, float velocity, float inaccuracy)
+    {
+        float f = -MathHelper.sin(yaw * 0.017453292F) * MathHelper.cos(pitch * 0.017453292F);
+        float f1 = -MathHelper.sin(pitch * 0.017453292F);
+        float f2 = MathHelper.cos(yaw * 0.017453292F) * MathHelper.cos(pitch * 0.017453292F);
+        this.setThrowableHeading((double)f, (double)f1, (double)f2, velocity, inaccuracy);
+        this.motionX += shooter.motionX;
+        this.motionZ += shooter.motionZ;
+
+        if (!shooter.onGround)
+        {
+            this.motionY += shooter.motionY;
+        }
+    }
+
+    /**
+     * Similar to setArrowHeading, it's point the throwable entity to a x, y, z direction.
+     */
+    @Override
+    public void setThrowableHeading(double x, double y, double z, float velocity, float inaccuracy)
+    {
+        float f = MathHelper.sqrt(x * x + y * y + z * z);
+        x = x / (double)f;
+        y = y / (double)f;
+        z = z / (double)f;
+        x = x + this.rand.nextGaussian() * 0.007499999832361937D * (double)inaccuracy;
+        y = y + this.rand.nextGaussian() * 0.007499999832361937D * (double)inaccuracy;
+        z = z + this.rand.nextGaussian() * 0.007499999832361937D * (double)inaccuracy;
+        x = x * (double)velocity;
+        y = y * (double)velocity;
+        z = z * (double)velocity;
+        this.motionX = x;
+        this.motionY = y;
+        this.motionZ = z;
+        float f1 = MathHelper.sqrt(x * x + z * z);
+        this.rotationYaw = (float)(MathHelper.atan2(x, z) * (180D / Math.PI));
+        this.rotationPitch = (float)(MathHelper.atan2(y, (double)f1) * (180D / Math.PI));
+        this.prevRotationYaw = this.rotationYaw;
+        this.prevRotationPitch = this.rotationPitch;
+        this.ticksInGround = 0;
+    }
+
+    protected void bulletHit(EntityLivingBase living)
+    {
+    }
+}
