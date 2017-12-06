d50faa719e1cc6a2347b5e2d47c1221a098a0b03
Hoijima <nenynax@gmail.com>
2017-12-07 01:54:46 +0300

    0.9.3.6.3

--- src/main/java/com/redsparkle/foe/Init/BlockInit.java
+++ src/main/java/com/redsparkle/foe/Init/BlockInit.java
@@ -1,12 +1,6 @@
 package com.redsparkle.foe.Init;
 
 import com.redsparkle.api.utils.GlobalNames;
-import com.redsparkle.foe.block.containers.SparkleColaMachineBlock;
-import com.redsparkle.foe.block.containers.TileEntitys.SparkleColaMachineTileEntity;
-import com.redsparkle.foe.block.effectDispenser.RadiationBlock;
-import com.redsparkle.foe.block.effectDispenser.TileEntitys.RadiationBlockTileEntity;
-import com.redsparkle.foe.block.interractable.*;
-import com.redsparkle.foe.block.interractable.TileEntitys.*;
 import com.redsparkle.foe.main;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -27,23 +21,13 @@ import java.util.Set;
 
 @Mod.EventBusSubscriber(modid = main.MODID)
 public class BlockInit {
-    public static final SparkleColaMachineBlock SparkleColaMachineBlock = new SparkleColaMachineBlock(GlobalNames.SPCmachine);
-    public static final workbench workbench = new workbench(GlobalNames.Workbench);
-    public static final workbench_handmade workbench_handmade = new workbench_handmade(GlobalNames.Workbench_handmade);
-    public static final armor_bench_tier_one armor_bench_tier_one = new armor_bench_tier_one(GlobalNames.ArmorBench_tier_one);
-    public static final DesktopTerminal DesktopTerminal = new DesktopTerminal(GlobalNames.Terminal);
-    public static final locker locker = new locker(GlobalNames.Locker);
-    public static final RadiationBlock RadiationBlock = new RadiationBlock(GlobalNames.RadBlock);
+//    public static final SparkleColaMachineBlock SparkleColaMachineBlock = new SparkleColaMachineBlock(GlobalNames.SPCmachine);
+
 
 
     public static final String[] blocksNames = new String[]{
-            GlobalNames.SPCmachine,
-            GlobalNames.Workbench,
-            GlobalNames.Workbench_handmade,
-            GlobalNames.ArmorBench_tier_one,
-            GlobalNames.Terminal,
-            GlobalNames.Locker,
-            GlobalNames.RadBlock
+//            GlobalNames.NAMES,
+
     };
 
     public static final Set<ItemBlock> ITEM_BLOCKS = new HashSet<>();
@@ -53,26 +37,14 @@ public class BlockInit {
         final IForgeRegistry<Block> registry = event.getRegistry();
 
         final Block[] blocks = {
-                SparkleColaMachineBlock,
-                workbench,
-                workbench_handmade,
-                armor_bench_tier_one,
-                DesktopTerminal,
-                locker,
-                RadiationBlock
+                //OBJECTS
         };
     }
 
     @SubscribeEvent
     public static void registerItemBlocks(final RegistryEvent.Register<Item> event) {
         final ItemBlock[] items = {
-                new ItemBlock(SparkleColaMachineBlock),
-                new ItemBlock(workbench),
-                new ItemBlock(workbench_handmade),
-                new ItemBlock(armor_bench_tier_one),
-                new ItemBlock(DesktopTerminal),
-                new ItemBlock(locker),
-                new ItemBlock(RadiationBlock)
+                // new ItemBlock(OBJECT),
 
         };
 
@@ -83,14 +55,8 @@ public class BlockInit {
 //            registry.register(items[i]);
             ITEM_BLOCKS.add(items[i]);
         }
-        //registry.registerAll(items);
-        registerTileEntity(SparkleColaMachineTileEntity.class, GlobalNames.SPCmachine);
-        registerTileEntity(DesktopTerminalTileEntity.class, GlobalNames.Terminal);
-        registerTileEntity(TileEntity_ArmorBench_tier_one.class, GlobalNames.ArmorBench_tier_one);
-        registerTileEntity(TileEntity_locker.class, GlobalNames.Locker);
-        registerTileEntity(TileEntity_workbench.class, GlobalNames.Workbench);
-        registerTileEntity(TileEntity_workbench_handmade.class, GlobalNames.Workbench_handmade);
-        registerTileEntity(RadiationBlockTileEntity.class, GlobalNames.RadBlock);
+        //registerTileEntity(items, GlobalNames.name);
+
     }
 
 
